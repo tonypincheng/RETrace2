f6f9461c37c2c4a3bd9e8f660cca10f859755e4f
Tony Cheng <tcheng@altoslabs.com>
2025-09-25 18:09:19 +0000

    update bootstrap annotation in phylo plot

--- modules/phylo/view_phylo.py
+++ modules/phylo/view_phylo.py
@@ -29,8 +29,9 @@ def setup_tree_style(show_bootstrap=False, circular=False):
     ts = ete3.TreeStyle()
     ts.show_leaf_name = False
     ts.show_branch_length = False
-    #ts.show_branch_support = show_bootstrap
-    ts.show_branch_support = False # set this to False becasue we are customizing the support text (see below)
+    ts.show_branch_support = show_bootstrap
+    ts.show_scale = False  # Disable the scale bar in corner
+    #ts.show_branch_support = False # set this to False becasue we are customizing the support text (see below)
     ts.mode = 'c' if circular else 'r'  # 'c' for circular, 'r' for rectangular
     #ts.branch_vertical_margin = 5
     
@@ -87,18 +88,18 @@ def style_tree_nodes(tree, sample_data, color_background=False):
             nstyle["size"] = 0
             
             # Customize support text to be larger
-            if node.support is not None:  # Check if support value exists
-                support_text = f"{node.support:.2f}  "  # add space after support text for better visualization
-                support_face = ete3.TextFace(support_text, fgcolor='#8B0000', fsize=support_text_size)
-                node.add_face(support_face, column=2, position='branch-bottom')  
-            node.set_style(nstyle)
+            # if node.support is not None:  # Check if support value exists
+            #     support_text = f"{node.support:.2f}  "  # add space after support text for better visualization
+            #     support_face = ete3.TextFace(support_text, fgcolor='#8B0000', fsize=support_text_size)
+            #     node.add_face(support_face, column=2, position='branch-bottom')  
+            # node.set_style(nstyle)
             
     return tree
 
 def render_tree(tree, prefix, tree_style):
     """Render tree to PDF and PNG files"""
     tree.render(f"{prefix}.viewPhylo.pdf", tree_style=tree_style, dpi=500)
-    tree.render(f"{prefix}.viewPhylo.png", tree_style=tree_style, dpi=500)
+    #tree.render(f"{prefix}.viewPhylo.png", tree_style=tree_style, dpi=500)
     
 
 def viewPhylo(samplesheet, tree_file, prefix, bootstrap, color_background=False, circular=False):
